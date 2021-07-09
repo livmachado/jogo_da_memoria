50b7e9063207427567a4eb85a8609e7d26b9560d
Lívia <71295710+livmachado@users.noreply.github.com>
2021-07-09 17:30:14 -0300

    Atualizei o codigo
    Criei uma estrutura e uma funcao de modo de jogo

--- jogodamemoria.c
+++ jogodamemoria.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
+
+typedef struct {
+    char nome [40];
+    int pontuacao;
+} Jogador;
+
 
 int logo(void);
 int menu(void);
+int modo_de_jogo (void);
 
 int main(void){
     logo();
@@ -14,6 +22,7 @@ int main(void){
 int logo(void){
 
     printf("\n\n");
+    printf("****************************************************************************************************************\n");
     printf("                               _________   __________   _________   __________                                  \n");
     printf("                              |______   | |   ____   | |   ______| |   ____   |                                 \n");
     printf("                                     |  | |  |    |  | |  |        |  |    |  |                                 \n");
@@ -36,8 +45,9 @@ int logo(void){
     printf("|  |        |  |  |  |          |  |        |  |  |  |    |  |  |  |      |   |        |  |       |   |    |   |\n");
     printf("|  |        |  |  |  |_______   |  |        |  |  |  |____|  |  |  |       |   |   ____|  |____   |   |    |   |\n");
     printf("|__|        |__|  |__________|  |__|        |__|  |__________|  |__|        |___| |____________|  |___|    |___|\n");
+    printf("****************************************************************************************************************\n");
     printf("\n\n");
-    printf("\t                                     Pressione o ENTRER para comecar                                        \n");
+    printf("\t                              Pressione o ENTRER para comecar                                               \n");
     getchar();
     system("cls");
 
@@ -47,21 +57,93 @@ int logo(void){
 int menu(void){
     int opcao;
     printf("\n\n");
-    printf("************************************************************\n");
-    printf("\t 1 - Iniciar\n\t 2 - Instrucao\n\t 3 - Ranking\n\t 4 - Sair\n");
-    printf("************************************************************\n");
+    printf("****************************************************************************************************************\n");
+    printf("\t\t\t 1 - Iniciar\n\t\t\t 2 - Instrucao\n\t\t\t 3 - Ranking\n\t\t\t 4 - Sair\n");
+    printf("****************************************************************************************************************\n");
 
     printf("\n Digite a opcao desejada: ");
     scanf ("%d", &opcao);
 
-    if (opcao>=5 || opcao <1){
-        system("cls");
-        printf("Escolha uma opcao valida.\n ");
-        printf("Aperte ENTRER para voltar. \n");
-        srand(time(NULL));
-        getchar();
-         
-        return menu();
+    switch (opcao){
+        case 1:
+            system("cls");
+            modo_de_jogo();
+            break;
+        case 2:
+            system("cls");
+            return 2;
+            break;
+        case 3:
+            system("cls");
+            return 3;
+            break;
+        case 4:
+            system("cls");
+            return 4;
+            break;    
+        default:
+            system("cls");
+            printf("****************************************************************************************************************\n");
+            printf("\t\t\t Escolha uma opcao valida.\n ");
+            printf("\t\t\t Aperte ENTRER para voltar. \n");
+            printf("****************************************************************************************************************\n");
+            system("pause > NUL");
+            if (getchar()){
+                system("cls");
+                return menu();
+            }
+            break;
     }
     return 0;
 }
+
+int modo_de_jogo (void){
+    int opcao;
+    Jogador jogador1, jogador2;
+    
+    printf("\n\n");
+    printf("****************************************************************************************************************\n");
+    printf("\t\t\t 1- Singleplayer (1 jogador)\n");
+    printf("\t\t\t 2- Multiplayer  (2 jogadores)\n");
+    printf("****************************************************************************************************************\n");
+    printf("\n\n");
+    printf(" Escolha um modo de jogo:");
+    scanf("%d", &opcao);
+
+    switch (opcao){
+        case 1:
+            system("cls");
+            printf("\n\n****************************************************************************************************************\n");
+            puts(" Digite seu nome:");
+            gets(jogador1.nome);
+            jogador1.pontuacao=0;
+            return 1;
+
+        case 2:
+            system("cls");
+            printf("\n\n****************************************************************************************************************\n");
+            puts(" Digite o nome do jogador 1: ");
+            gets(jogador1.nome);
+            jogador1.pontuacao=0;
+            puts("\t\t\t Digite o nome do jogador 2: ");
+            gets(jogador2.nome);
+            jogador2.pontuacao=0;
+            return 2;
+
+        default:
+            system("cls");
+            printf("****************************************************************************************************************\n");
+            printf("\t\t\t Escolha uma opcao valida.\n ");
+            printf("\t\t\t Aperte ENTRER para voltar. \n");
+            printf("****************************************************************************************************************\n");
+            system("pause > NUL");
+            if (getchar()){
+                system("cls");
+                return modo_de_jogo();
+            }
+            break;
+    }
+    return 0;
+}
+
+
